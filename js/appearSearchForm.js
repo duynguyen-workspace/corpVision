9bc33ab3a53242b8494e1c3518dfded977158131
duynguyen-workspace <duynguyen.workspace@gmail.com>
2023-05-01 09:42:38 +0700

    add comments on js

--- js/appearSearchForm.js
+++ js/appearSearchForm.js
@@ -1,8 +1,10 @@
+// Get all the elements from the search box and form
 var searchBtn = document.getElementById("corpSearch");
 var closeBtn = document.getElementById("closeSearch");
 var searchWrapper = document.getElementById("searchWrapper");
 var searchBox = document.getElementById("searchBox")
 
+// Appear the search form when user click the search button
 function appearForm() {
     closeBtn.classList.add("show");
     searchWrapper.classList.add("grow");
@@ -10,6 +12,7 @@ function appearForm() {
     searchBox.classList.add("show");
 }
 
+// Close the search form when user click the close button
 function closeForm() {
     closeBtn.classList.remove("show");
     searchWrapper.classList.remove("grow");
